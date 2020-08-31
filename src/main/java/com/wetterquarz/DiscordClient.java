30b6b9043b47505a84c10ae5808bc548af9b6c9c
Wetterquarz <cedric.lippke@hotmail.de>
2020-08-31 20:25:39 +0200

    Integrated PluginManager in DiscordClient

--- src/main/java/com/wetterquarz/DiscordClient.java
+++ src/main/java/com/wetterquarz/DiscordClient.java
@@ -1,20 +1,25 @@
 package com.wetterquarz;
 
 import com.wetterquarz.config.ConfigHandler;
+import com.wetterquarz.plugin.PluginManager;
+
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.EventDispatcher;
+import reactor.core.publisher.Mono;
 
 public class DiscordClient {
 	
 	private final discord4j.core.DiscordClient client;
 	private final Object commandManager = null;
-	private final Object pluginManager = null;
+	private final PluginManager pluginManager;
 	private final Object databaseManager = null;
 	private GatewayDiscordClient gatewayDiscordClient = null;
+	private final Mono<Void> onDisconnect;
 	
 	private DiscordClient(String token) {
 		this.client = discord4j.core.DiscordClient.create(token);
-    	this.client.withGateway(gateway -> {
+		this.pluginManager = new PluginManager();
+    	this.onDisconnect = this.client.withGateway(gateway -> {
     		gatewayDiscordClient = gateway;
     		
     		return gateway.onDisconnect();
@@ -36,6 +41,8 @@ public class DiscordClient {
     		return;
 
     	discordClient = new DiscordClient(config.getString("token"));
+    	
+    	discordClient.onDisconnect.block();
     }
     
     public static DiscordClient getDiscordClient() {
