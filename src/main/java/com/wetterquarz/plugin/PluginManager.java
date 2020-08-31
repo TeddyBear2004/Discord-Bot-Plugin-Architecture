0f91216559edf4786a420c74ce5a8effab0db4dd
Wetterquarz <cedric.lippke@hotmail.de>
2020-08-31 20:33:05 +0200

    Removed unused imports

--- src/main/java/com/wetterquarz/plugin/PluginManager.java
+++ src/main/java/com/wetterquarz/plugin/PluginManager.java
@@ -1,15 +1,12 @@
 package com.wetterquarz.plugin;
 
 import com.wetterquarz.config.ConfigHandler;
-import com.wetterquarz.plugin.events.PluginStartEvent;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.*;
