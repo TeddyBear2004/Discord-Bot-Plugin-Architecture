2a31ffaf97d1cf94a2361cc5077c00605074cb80
TeddyBear2004 <thorgeengelmann@gmail.com>
2021-10-02 14:47:45 +0200

    fixed bug that uppercase subcommands are not executing

--- src/main/java/com/wetterquarz/command/CommandSegment.java
+++ src/main/java/com/wetterquarz/command/CommandSegment.java
@@ -1,5 +1,6 @@
 package com.wetterquarz.command;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.BiConsumer;
 
@@ -11,6 +12,7 @@ import com.google.common.collect.ImmutableMap;
 public class CommandSegment {
     @NotNull final CommandExecutable commandExecutable;
     @Nullable final Map<String, CommandSegment> commandSegments;
+    @Nullable final Map<String, CommandSegment> commandSegmentsLowerCase;
 
     public CommandSegment(@Nullable Map<String, CommandSegment> commandSegments, @Nullable CommandExecutable commandExecutable){
 
@@ -22,19 +24,32 @@ public class CommandSegment {
                 throw new IllegalArgumentException();
         }catch(NullPointerException ignore){
         }finally{
+            if(commandSegments == null){
+                commandSegmentsLowerCase = null;
+            }else{
+                Map<String, CommandSegment> commandSegmentsLowerCase = new HashMap<>();
+                commandSegments.forEach((s, commandSegment) -> commandSegmentsLowerCase.put(s.toLowerCase(), commandSegment));
+                this.commandSegmentsLowerCase = commandSegmentsLowerCase;
+            }
+
             this.commandSegments = commandSegments == null ? null : ImmutableMap.copyOf(commandSegments);
             this.commandExecutable = commandExecutable == null ? new DefaultCommandExecutable(this) : commandExecutable;
         }
     }
 
     public void forEachPossibleArgument(BiConsumer<String, CommandSegment> biConsumer){
-        if(commandSegments == null)return;
+        if(commandSegments == null)
+            return;
 
         commandSegments.forEach(biConsumer);
     }
 
     public @Nullable Map<String, CommandSegment> getCommandSegments(){
         return commandSegments;
+    }
+
+    public Map<String, CommandSegment> getCommandSegmentsLowerCase(){
+        return commandSegmentsLowerCase;
     }
 
     public @NotNull CommandExecutable getExecutableCommand(){
