d7b213e67bd288d500dc7775d38a19211ba3d7a9
TeddyBear2004 <thorgeengelmann@gmail.com>
2021-10-31 11:50:51 +0100

    made getter public

--- src/main/java/com/wetterquarz/plugin/PluginMetadata.java
+++ src/main/java/com/wetterquarz/plugin/PluginMetadata.java
@@ -2,32 +2,40 @@ package com.wetterquarz.plugin;
 
 public class PluginMetadata {
 
-	private Plugin plugin;
-	private String name;
-	private String label;
-	private String version;
-	Plugin getPlugin() {
-		return plugin;
-	}
-	void setPlugin(Plugin plugin) {
-		this.plugin = plugin;
-	}
-	String getName() {
-		return name;
-	}
-	void setName(String name) {
-		this.name = name;
-	}
-	String getLabel() {
-		return label;
-	}
-	void setLabel(String label) {
-		this.label = label;
-	}
-	String getVersion() {
-		return version;
-	}
-	void setVersion(String version) {
-		this.version = version;
-	}
+    private Plugin plugin;
+    private String name;
+    private String label;
+    private String version;
+
+    public Plugin getPlugin(){
+        return plugin;
+    }
+
+    void setPlugin(Plugin plugin){
+        this.plugin = plugin;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    void setName(String name){
+        this.name = name;
+    }
+
+    public String getLabel(){
+        return label;
+    }
+
+    void setLabel(String label){
+        this.label = label;
+    }
+
+    public String getVersion(){
+        return version;
+    }
+
+    void setVersion(String version){
+        this.version = version;
+    }
 }
