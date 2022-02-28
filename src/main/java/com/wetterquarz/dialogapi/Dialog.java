584e87b170e378358af900c55e128402470caa26
TeddyBear2004 <thorgeengelmann@gmail.com>
2022-02-28 21:30:12 +0100

    added more intuitive use of Dialog.Builder

--- src/main/java/com/wetterquarz/dialogapi/Dialog.java
+++ src/main/java/com/wetterquarz/dialogapi/Dialog.java
@@ -2,13 +2,19 @@ package com.wetterquarz.dialogapi;
 
 import discord4j.core.object.entity.User;
 import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.object.reaction.ReactionEmoji;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.core.spec.MessageCreateSpec;
+import org.jetbrains.annotations.NotNull;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.temporal.TemporalUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 
 public class Dialog {
     private final List<DialogSegment<Object>> dialogSegments;
@@ -41,9 +47,33 @@ public class Dialog {
             return this;
         }
 
+        public Builder text(String text) {
+            return this.addSegment(new TextSegment(text));
+        }
+
+        public Builder text(EmbedCreateSpec embedCreateSpec) {
+            return this.addSegment(new TextSegment(embedCreateSpec));
+        }
+
+        public Builder text(MessageCreateSpec messageCreateSpec) {
+            return this.addSegment(new TextSegment(messageCreateSpec));
+        }
+
+        public Builder multipleChoiceSegment(@NotNull EmbedCreateSpec.Builder spec, @NotNull Function<String, String> description, @NotNull Map<ReactionEmoji, ?> options) {
+            return this.addSegment(new MultipleChoiceSegment(spec, description, options));
+        }
+
         public Builder addConditionalSegment(DialogSegment<Boolean> booleanDialogSegment, Builder ifTrue, Builder ifFalse) {
             this.dialogSegments.add(new ConditionalSegment(booleanDialogSegment, ifTrue, ifFalse));
             return this;
+        }
+
+        public Builder addQuestion(Function<String, String> description, EmbedCreateSpec.Builder builder) {
+            return this.addSegment(new QuestionSegment(description, builder));
+        }
+
+        public Builder addQuestion(Function<String, String> description) {
+            return this.addSegment(new QuestionSegment(description));
         }
 
         public Builder wait(int time, TemporalUnit unit) {
